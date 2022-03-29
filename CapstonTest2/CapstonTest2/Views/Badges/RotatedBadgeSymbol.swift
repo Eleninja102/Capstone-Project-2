a9c9dfde454acc9c3a65ef474f51446f7ffe7ebf
Coleton Watt <electricninja102@gmail.com>
2022-03-28 18:46:00 -0600

    Added Some More

---
+++ CapstonTest2/CapstonTest2/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  CapstonTest2
+//
+//  Created by Coleton Watt on 3/28/22.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 15))
+    }
+}
